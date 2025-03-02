73eca9a0512ccdde92705c55ccfd2ab42b084ad1
Satoshi T <64382011+Satoshi-Tb@users.noreply.github.com>
2025-03-02 16:46:21 +0900

    タスク追加、タスク表示のエンドポイント追加

--- src/main/java/jp/gihyo/projava/tasklist/HomeRestController.java
+++ src/main/java/jp/gihyo/projava/tasklist/HomeRestController.java
@@ -1,12 +1,37 @@
 package jp.gihyo.projava.tasklist;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 @RestController
 public class HomeRestController {
+
+    record TaskItem(String id, String task, String deadline, boolean done) {}
+    private List<TaskItem> taskItems = new ArrayList<>();
+
+    @GetMapping("/restadd")
+    String addItem(@RequestParam("task") String task, @RequestParam("deadline") String deadline) {
+        String id = UUID.randomUUID().toString().substring(0, 8);
+        var item = new TaskItem(id, task, deadline, false);
+        taskItems.add(item);
+
+        return "タスクを追加しました";
+    }
+
+    @GetMapping("/restlist")
+    String listItems() {
+        return taskItems.stream().map(TaskItem::toString).collect(Collectors.joining());
+    }
+
+
     @RequestMapping("/resthello")
     String hello() {
         return """
